a22e20e04a38d4b4cafef49267a1d459a9c38b66
codesmartfb-oss <codesmartfb@gmail.com>
2026-09-04 13:19:07 +0000

    chore(prod): remove duplicated legacy tree

--- catalogue_whatsapp/api/auth.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/bootstrap.php';
-$input = body();
-if (($input['email'] ?? '') === 'admin@katalog.local' && ($input['password'] ?? '') === 'change-me-now') out(['token' => adminToken()]);
-out(['error' => 'Identifiants invalides'], 401);
